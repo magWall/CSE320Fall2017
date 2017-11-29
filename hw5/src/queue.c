8c6ff3e988128c5ad565c4c21254c0afe4d923a5
Kenny Lee <kennylee@cs.stonybrook.edu>
2017-11-28 23:19:01 -0500

    Create_queue finished[?], started enqueue

--- hw5/src/queue.c
+++ hw5/src/queue.c
@@ -1,7 +1,13 @@
 #include "queue.h"
+#include "errno.h"
 
 queue_t *create_queue(void) {
-    return NULL;
+    queue_t* newQueue = calloc(1,sizeof(queue_t));
+    if(newQueue == NULL) //won't be able to access pointer if NULL
+        return NULL;
+    sem_init(&(newQueue->items),0,1); //initalize to 1
+    pthread_mutex_init(&(newQueue->lock),NULL); //lock, attr
+    return newQueue;
 }
 
 bool invalidate_queue(queue_t *self, item_destructor_f destroy_function) {
@@ -9,9 +15,41 @@ bool invalidate_queue(queue_t *self, item_destructor_f destroy_function) {
 }
 
 bool enqueue(queue_t *self, void *item) {
+    if(self == NULL || item == NULL)
+    {
+        errno = EINVAL;
+        return false;
+    }
+    //lock
+    if (self->front == NULL)
+    {
+        //sem
+        //add to queue
+        //unsem
+    }
+    else if(self ->front == NULL)//second item in list, add to rear, link front and rear
+    {
+        //sem
+        //add to queue
+        //unsem
+    }
+    else //not second item, replace rear with item, link prev. rear to new rear
+    {
+        //sem
+        //add to queue
+        //unsem
+    }
+    //unlock
+
     return false;
 }
 
 void *dequeue(queue_t *self) {
+    if(self == NULL)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
     return NULL;
 }
